6e71a89c1f0b01bf9787d7b17ef6af92808232e3
Anfernee Rapio <rapioanfernee@gmail.com>
2019-10-28 15:29:19 +0800

    Updates

--- precache-manifest.a036fb7c109460a23f1929a74a2c5684.js
+++ precache-manifest.a036fb7c109460a23f1929a74a2c5684.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4ecdd7028920927a0b13f9a858c679b4",
+    "revision": "546325ac4d8a8d738d7dca92f68efd51",
     "url": "/portfolio/index.html"
   },
   {
@@ -8,15 +8,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/css/2.0624914a.chunk.css"
   },
   {
-    "revision": "e666dcf4e0efbb3f14ea",
-    "url": "/portfolio/static/css/main.07ade703.chunk.css"
+    "revision": "c79c093182d49de20c99",
+    "url": "/portfolio/static/css/main.d3e9ab65.chunk.css"
   },
   {
     "revision": "355ce146e9109fdbe06e",
     "url": "/portfolio/static/js/2.ff1abcca.chunk.js"
   },
   {
-    "revision": "e666dcf4e0efbb3f14ea",
+    "revision": "c79c093182d49de20c99",
     "url": "/portfolio/static/js/main.725e1c26.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.1dc125c1abf09296219ff1ee2c04f530.js"
+  "/portfolio/precache-manifest.a036fb7c109460a23f1929a74a2c5684.js"
 );
 
 self.addEventListener('message', (event) => {
